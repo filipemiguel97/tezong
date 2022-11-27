b483f77a52900150d05d87e6f09b67d973da182e
filipemiguel97 <fsdrm2@cam.ac.uk>
2022-11-27 10:50:03 +0000

    Taquito initial steps

---
+++ website/static/js/taquito_interface.js
@@ -0,0 +1,15 @@
+class smart_table {
+    constructor(player_defender, player_challenger, smart_contract_address){
+        this.player_defender = player_defender;
+        this.player_challenger = player_challenger;
+        this.smart_contract_address=smart_contract_address;
+    }
+
+    buy_in_challenger(){
+
+    }
+
+    defende
+
+
+}
